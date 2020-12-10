85d0ba26c36e1804fe8a543860bcbccd120fa9d8
mkmkkim <0098121@naver.com>
2020-12-10 17:58:42 +0900

    edit
    1. iframe 내에 페이지가 뜨지 않는 문제 해결.
    2. 커서문제 해결 but 새로운 이미지 파일 만들 예정

--- search.js
+++ search.js
@@ -1,51 +1,50 @@
 function enterSearch() {
-    if(event.keyCode == 13){
-        myFunction();  // 실행할 이벤트
+  if (event.keyCode == 13) {
+    myFunction(); // 실행할 이벤트
     }
+  }
+function myFunction(event) {
+  event.preventDefault();
+  var x = document.getElementById("texts").value;
+  var frame = document.getElementById("frame");
+    
+if (x == "apple") {
+  frame.setAttribute("src", "domestic_fruits.html");
 }
-function myFunction() {
-    var x = document.getElementById("texts").value;
-
-    if (x=="apple"){
-        window.location.href = "domestic_fruits.html";
-    }
-    else if(x=="strawberry"){
-        window.location.href = "domestic_fruits.html";
-    }
-    else if(x=="grape"){
-        window.location.href = "domestic_fruits.html";
-    }
-    else if(x=="pear"){
-        window.location.href = "domestic_fruits.html";
-    }
-    else if(x=="plum"){
-        window.location.href = "domestic_fruits.html";
-    }
-    else if(x=="peach"){
-        window.location.href = "domestic_fruits.html";
-    }
-    else{
-        window.location.href = "not_found.html";
-    }
-    if(x=="pineapple"){
-        window.location.href = "foreign_fruits.html";
-    }
-    else if(x=="blueberry"){
-        window.location.href = "foreign_fruits.html";
-    }
-    else if(x=="melon"){
-        window.location.href = "foreign_fruits.html";
-    }
-    else if(x=="cherry"){
-        window.location.href = "foreign_fruits.html";
-    }
-    else if(x=="grapefruit"){
-        window.location.href = "foreign_fruits.html";
-    }
-    else if(x=="mango"){
-        window.location.href = "foreign_fruits.html";
-    }
-    else{
-        window.location.href = "not_found.html";
-    }
+else if (x == "strawberry"){
+  frame.setAttribute("src", "domestic_fruits.html");
+}
+else if (x == "grape") {
+  frame.setAttribute("src", "domestic_fruits.html");
+}
+else if (x == "pear") {
+  frame.setAttribute("src", "domestic_fruits.html");
+}
+else if (x == "plum") {
+  frame.setAttribute("src", "domestic_fruits.html");
+}
+else if (x == "peach") {
+  frame.setAttribute("src", "domestic_fruits.html");
+}
+else if (x == "pineapple") {
+  frame.setAttribute("src", "foreign_fruits.html");
+}
+else if (x == "blueberry") {
+  frame.setAttribute("src", "foreign_fruits.html");
+}
+else if (x == "melon") {
+  frame.setAttribute("src", "foreign_fruits.html");
+}
+else if (x == "cherry") {
+  frame.setAttribute("src", "foreign_fruits.html");
+}
+else if (x == "grapefruit") {
+  frame.setAttribute("src", "foreign_fruits.html");
+}
+else if (x == "mango") {
+  frame.setAttribute("src", "foreign_fruits.html");
+}
+else {
+  frame.setAttribute("src", "not_found.html");
+}
 }
